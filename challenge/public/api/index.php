0524ce8c04698e31d42ba135edf2bb4371b5ac6f
pavpench <p.penchev90@gmail.com>
2022-10-11 20:09:37 +0200

    initial setup of route for user data storage

--- challenge/public/api/index.php
+++ challenge/public/api/index.php
@@ -17,19 +17,53 @@ AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->setBasePath('/api');
 $app->addRoutingMiddleware();
+
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
-/**
- * TODO
+
+/** TODO
  * Here you can write your own API endpoints.
- * You can use Redis and/or cookies for data persistence.
- *
- * Find below an example of a GET endpoint that uses redis to temporarily store a name,
- * and cookies to keep track of an event date and time.
- */
+ * You can use Redis and/or cookies for data persistence. */
 
-$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
+ 
+ //Soft login for storing initial information of User 
+$app->post("/login/{name}",function(Request $request, Response $response, $args){
+    
+    $loggedName = $args["name"];
+    $redisClient = $this->get("redisClient");
+    $userData = $redisClient->hgetall($loggedName);
+    $reqBody = $request->getParsedBody();
+    $timeStarted = $reqBody["timeStart"];
+    
+     if($userData["userName"]){
+        $response->getBody()->write(json_encode([$loggedName=>$userData,"salutation"=>"Hello $loggedName"], JSON_THROW_ON_ERROR));
+    
+    }else{
+        $redisClient->hmset($loggedName,
+        "userName", $loggedName,
+        "startReadTime",$timeStarted,"currentReadTime","00:00:00","endReadTime","00:00:00");
+        
+        $response->getBody()->write(json_encode(["response"=>"Thank you for logging in"]));
+        
+    };
+    return $response->withHeader("Content-Type","application/json");
+});
+// Storing info for session time in case of not full read
+$app->post("/sessionOver/{name}",function(Request $request,Response $response, $args){
+    
+});
+// Providing details for startTime, endTime, overallTimeSpent
+$app->get("/finnishedRead/{name}",function(Request $request,Response $response,$args){
+    
+});
 
+
+
+/*Find below an example of a GET endpoint that uses redis to temporarily store a name,
+ and cookies to keep track of an event date and time. */
+
+
+ $app->get('/hello/{name}', function (Request $request, Response $response, $args) {
     // Redis usage example:
     /** @var \Predis\Client $redisClient */
     $redisClient = $this->get('redisClient');
@@ -59,5 +93,7 @@ $app->get('/hello/{name}', function (Request $request, Response $response, $args
 
     return $response->withHeader('Content-Type', 'application/json');
 });
+
+
 
 $app->run();
